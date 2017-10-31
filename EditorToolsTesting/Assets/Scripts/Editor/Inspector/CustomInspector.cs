b85a4faa3dead8b288bd3c90b70cf9aba477bb64
leli0019 <jason.lelievre96@gmail.com>
2017-10-31 11:28:19 -0400

    Prototyped Custom Inspector

---
+++ EditorToolsTesting/Assets/Scripts/Editor/Inspector/CustomInspector.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(CustomScript))]
+public class CustomInspector : Editor {
+
+    CustomScript yourscript;
+
+    bool showFloats;
+    private void OnEnable()
+    {
+        yourscript = (CustomScript)target;
+        showFloats = false;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        if( showFloats = GUILayout.Toggle(showFloats, "Show/Hide", GUI.skin.button))
+        {
+            yourscript.testfloat1 = EditorGUILayout.FloatField("TestingFloat", yourscript.testfloat1);
+            yourscript.testfloat2 = EditorGUILayout.FloatField("TestingFloat", yourscript.testfloat2);
+        }
+    }
+
+}
---
+++ EditorToolsTesting/Assets/Scripts/MonoBehaviour/Inspector/CustomScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomScript : MonoBehaviour {
+
+    [HideInInspector]
+    public float testfloat1;
+    [HideInInspector]
+    public float testfloat2;
+
+    public string helloworld;   
+    public bool isTesting;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
